d9a840a02cd2781502dcff3a76f9c936affc33b1
WangChengHao2000 <2694681433@qq.com>
2023-09-23 16:22:46 +0800

    fix controller

--- controller.py
+++ controller.py
@@ -3,16 +3,15 @@ import random
 
 
 class Controller(object):
-
     def __init__(self):
         print("Controller init...")
 
     def run(self, image, steering, throttle):
         print("get input")
         print(type(image))
-        steering+=random.randint(0,10)
-        throttle+=random.randint(0,10)
-        print('steering: %d' % steering)
-        print('throttle: %d' % throttle)
+        steering += random.randint(0, 10)
+        throttle += random.randint(0, 10)
+        print("steering: %d" % steering)
+        print("throttle: %d" % throttle)
         time.sleep(0.3)
-        return
+        return [steering, throttle]
