86c48631abdeb49317a2def66e2e2d714d27a6e8
WangChengHao2000 <2694681433@qq.com>
2023-09-25 14:49:15 +0800

    add logger

--- controller.py
+++ controller.py
@@ -1,20 +1,29 @@
 import time
 import random
 
+from donkeycar.utils import logging
+
+logger = logging.getLogger(__name__)
+
 
 class Controller(object):
     def __init__(self):
-        print("Controller init...")
+        logger.info("Controller init...")
+        self.right = True
 
     def run(self, image, steering, throttle):
-        print("get input")
-        print(type(image))
-        steering += random.randint(0, 10)
-        throttle += random.randint(0, 10)
-        print("steering: %d" % steering)
-        print("throttle: %d" % throttle)
-        time.sleep(0.3)
+        logger.info(f'get image input: {str(type(image))}')
+        if self.right:
+            steering += 0.1
+        else:
+            steering -= 0.1
+        if steering > 1 or steering < -1:
+            self.right = not self.right
+        # throttle += random.randint(-3, 3)
+        logger.info(f'steering: {steering}')
+        logger.info(f'throttle: {throttle}')
         return [steering, throttle]
+
 
 class BackendController(object):
     def __init__(self):
@@ -35,6 +44,7 @@ if __name__ == "__main__":
     from PIL import Image
     import numpy as np
 
-    controller = BackendController()
+    controller = Controller()
+    # controller = BackendController()
     image = np.array(Image.new("RGB", (256, 256)))
     controller.run(image=image, steering=20, throttle=300)
--- manage.py
+++ manage.py
@@ -1,59 +1,93 @@
 import donkeycar as dk
 from donkeycar.parts.transform import Lambda
+from donkeycar.utils import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 
 def run(cfg):
     V = dk.vehicle.Vehicle()
 
+    if cfg.HAVE_CONSOLE_LOGGING:
+        logger.setLevel(logging.getLevelName(cfg.LOGGING_LEVEL))
+        ch = logging.StreamHandler()
+        ch.setFormatter(logging.Formatter(cfg.LOGGING_FORMAT))
+        logger.addHandler(ch)
+
+    add_simulator(V, cfg)
     add_camera(V, cfg)
     add_controller(V, cfg)
     add_drive(V, cfg)
 
-    V.start(rate_hz=3, max_loop_count=10)
+    V.start(rate_hz=10, max_loop_count=100)
+
+
+def add_simulator(V, cfg):
+    if cfg.DONKEY_GYM:
+        from donkeycar.parts.dgym import DonkeyGymEnv
+
+        gym = DonkeyGymEnv(
+            cfg.DONKEY_SIM_PATH,
+            host=cfg.SIM_HOST,
+            env_name=cfg.DONKEY_GYM_ENV_NAME,
+            conf=cfg.GYM_CONF,
+        )
+
+        V.add(
+            gym,
+            inputs=["steering", "throttle"],
+            outputs=["cam/image_array"],
+            threaded=True,
+        )
 
 
 def add_camera(V, cfg):
-    if cfg.CAMERA_TYPE == "PICAM":
-        from donkeycar.parts.camera import PiCamera
+    if not cfg.DONKEY_GYM:
+        if cfg.CAMERA_TYPE == "PICAM":
+            from donkeycar.parts.camera import PiCamera
 
-        cam = PiCamera(
-            image_w=cfg.IMAGE_W,
-            image_h=cfg.IMAGE_H,
-            image_d=cfg.IMAGE_DEPTH,
-            vflip=cfg.CAMERA_VFLIP,
-            hflip=cfg.CAMERA_HFLIP,
-        )
-        V.add(cam, inputs=[], outputs=["image"], threaded=True)
-    elif cfg.CAMERA_TYPE == "MOCK":
-        from donkeycar.parts.camera import MockCamera
+            cam = PiCamera(
+                image_w=cfg.IMAGE_W,
+                image_h=cfg.IMAGE_H,
+                image_d=cfg.IMAGE_DEPTH,
+                vflip=cfg.CAMERA_VFLIP,
+                hflip=cfg.CAMERA_HFLIP,
+            )
+            V.add(cam, inputs=[], outputs=["cam/image_array"], threaded=True)
+        elif cfg.CAMERA_TYPE == "MOCK":
+            from donkeycar.parts.camera import MockCamera
 
-        cam = MockCamera(
-            image_w=cfg.IMAGE_W, image_h=cfg.IMAGE_H, image_d=cfg.IMAGE_DEPTH
-        )
-        V.add(cam, inputs=[], outputs=["image"], threaded=True)
-    else:
-        raise (Exception("Unknown camera type: %s" % cfg.CAMERA_TYPE))
+            cam = MockCamera(
+                image_w=cfg.IMAGE_W, image_h=cfg.IMAGE_H, image_d=cfg.IMAGE_DEPTH
+            )
+            V.add(cam, inputs=[], outputs=["cam/image_array"], threaded=True)
+        else:
+            raise (Exception("Unknown camera type: %s" % cfg.CAMERA_TYPE))
 
 
 def add_controller(V, cfg):
+    if not cfg.DONKEY_GYM:
+        V.mem["steering"] = int((cfg.STEERING_LEFT_PWM + cfg.STEERING_RIGHT_PWM) / 2)
+        V.mem["throttle"] = cfg.THROTTLE_STOPPED_PWM
+    else:
+        V.mem["steering"] = 0
+        V.mem["throttle"] = 0.1
+
     if cfg.CONTROLLER_TYPE == "MOCK":
         from controller import Controller
 
-        V.mem["steering"] = int((cfg.STEERING_LEFT_PWM + cfg.STEERING_RIGHT_PWM) / 2)
-        V.mem["throttle"] = cfg.THROTTLE_STOPPED_PWM
         V.add(
             Controller(),
-            inputs=["image", "steering", "throttle"],
+            inputs=["cam/image_array", "steering", "throttle"],
             outputs=["steering", "throttle"],
         )
     elif cfg.CONTROLLER_TYPE == "BACKEND":
         from controller import BackendController
 
-        V.mem["steering"] = int((cfg.STEERING_LEFT_PWM + cfg.STEERING_RIGHT_PWM) / 2)
-        V.mem["throttle"] = cfg.THROTTLE_STOPPED_PWM
         V.add(
             BackendController(),
-            inputs=["image", "steering", "throttle"],
+            inputs=["cam/image_array", "steering", "throttle"],
             outputs=["steering", "throttle"],
         )
     else:
@@ -61,39 +95,48 @@ def add_controller(V, cfg):
 
 
 def add_drive(V, cfg):
-    if cfg.DRIVE_TRAIN_TYPE == "PWM_STEERING_THROTTLE":
-        from donkeycar.parts import pins
-        from donkeycar.parts.actuator import PWMSteering, PWMThrottle, PulseController
+    if not cfg.DONKEY_GYM:
+        if cfg.DRIVE_TRAIN_TYPE == "PWM_STEERING_THROTTLE":
+            from donkeycar.parts import pins
+            from donkeycar.parts.actuator import (
+                PWMSteering,
+                PWMThrottle,
+                PulseController,
+            )
 
-        dt = cfg.PWM_STEERING_THROTTLE
-        steering_controller = PulseController(
-            pwm_pin=pins.pwm_pin_by_id(dt["PWM_STEERING_PIN"]),
-            pwm_scale=dt["PWM_STEERING_SCALE"],
-            pwm_inverted=dt["PWM_STEERING_INVERTED"],
-        )
-        steering = PWMSteering(
-            controller=steering_controller,
-            left_pulse=dt["STEERING_LEFT_PWM"],
-            right_pulse=dt["STEERING_RIGHT_PWM"],
-        )
-        throttle_controller = PulseController(
-            pwm_pin=pins.pwm_pin_by_id(dt["PWM_THROTTLE_PIN"]),
-            pwm_scale=dt["PWM_THROTTLE_SCALE"],
-            pwm_inverted=dt["PWM_THROTTLE_INVERTED"],
-        )
-        throttle = PWMThrottle(
-            controller=throttle_controller,
-            max_pulse=dt["THROTTLE_FORWARD_PWM"],
-            zero_pulse=dt["THROTTLE_STOPPED_PWM"],
-            min_pulse=dt["THROTTLE_REVERSE_PWM"],
-        )
-        V.add(steering, inputs=["steering"], threaded=True)
-        V.add(throttle, inputs=["throttle"], threaded=True)
-    elif cfg.DRIVE_TRAIN_TYPE == "MOCK":
-        V.add(Lambda(lambda v: print("execute steering: %d" % v)), inputs=["steering"])
-        V.add(Lambda(lambda v: print("execute throttle: %d" % v)), inputs=["throttle"])
-    else:
-        raise (Exception("Unknown drive type: %s" % cfg.DRIVE_TRAIN_TYPE))
+            dt = cfg.PWM_STEERING_THROTTLE
+            steering_controller = PulseController(
+                pwm_pin=pins.pwm_pin_by_id(dt["PWM_STEERING_PIN"]),
+                pwm_scale=dt["PWM_STEERING_SCALE"],
+                pwm_inverted=dt["PWM_STEERING_INVERTED"],
+            )
+            steering = PWMSteering(
+                controller=steering_controller,
+                left_pulse=dt["STEERING_LEFT_PWM"],
+                right_pulse=dt["STEERING_RIGHT_PWM"],
+            )
+            throttle_controller = PulseController(
+                pwm_pin=pins.pwm_pin_by_id(dt["PWM_THROTTLE_PIN"]),
+                pwm_scale=dt["PWM_THROTTLE_SCALE"],
+                pwm_inverted=dt["PWM_THROTTLE_INVERTED"],
+            )
+            throttle = PWMThrottle(
+                controller=throttle_controller,
+                max_pulse=dt["THROTTLE_FORWARD_PWM"],
+                zero_pulse=dt["THROTTLE_STOPPED_PWM"],
+                min_pulse=dt["THROTTLE_REVERSE_PWM"],
+            )
+            V.add(steering, inputs=["steering"], threaded=True)
+            V.add(throttle, inputs=["throttle"], threaded=True)
+        elif cfg.DRIVE_TRAIN_TYPE == "MOCK":
+            V.add(
+                Lambda(lambda v: print("execute steering: %d" % v)), inputs=["steering"]
+            )
+            V.add(
+                Lambda(lambda v: print("execute throttle: %d" % v)), inputs=["throttle"]
+            )
+        else:
+            raise (Exception("Unknown drive type: %s" % cfg.DRIVE_TRAIN_TYPE))
 
 
 if __name__ == "__main__":
