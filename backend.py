900cc63efeff0b09410415a0199595b96ef5e76f
WangChengHao2000 <2694681433@qq.com>
2023-10-08 22:28:22 +0800

    modify backend

--- backend.py
+++ backend.py
@@ -6,10 +6,10 @@ if __name__ == "__main__":
 
     @app.route("/get", methods=["POST"])
     def get():
-        steering = int(request.form["steering"])
-        throttle = int(request.form["throttle"])
-        steering += random.randint(-10, 10)
-        throttle += random.randint(-100, 100)
+        steering = float(request.form["steering"])
+        throttle = float(request.form["throttle"])
+        steering += random.randint(-1, 1) * 0.1
+        throttle += random.randint(-1, 1) * 0.1
         return {"steering": steering, "throttle": throttle}
 
     app.run(host="127.0.0.1", port=8887)
